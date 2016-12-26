b06b1dc1c974731d568d4fba39aad695c0d1664a
Sruthisivakumar3 <sruthisiva3@gmail.com>
2016-12-26 22:17:29 +0530

    Rename digit.c to digits.c

---
+++ digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int num,r,count=0,sum = 0;
+  do 
+  { 
+    r=num % 10;
+    sum=sum+r; 
+    num=num/10;
+    count++;
+  } while (num > 0); 
+    printf("The sum of digits is %d",sum);
+    printf("The number of digits are %d",count);
+}
